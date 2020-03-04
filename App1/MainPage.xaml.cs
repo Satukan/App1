3c7bf4f6127a48de52dba16890a434cf061e8044
Piroska Istvan <satukan@gmail.com>
2020-03-04 15:45:59 +0100

    Add project files.

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public ICommand TestCommand => new RelayCommand<object>(p => Test());
+        private void Test()
+        {
+
+        }
+
+        public class RelayCommand<T> : ICommand where T : class
+        {
+            private readonly Action<object> commandTask;
+
+            public RelayCommand(Action<object> workToDo)
+            {
+                commandTask = workToDo;
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public void Execute(object parameter)
+            {
+                commandTask(parameter);
+            }
+        }
+    }
+}
